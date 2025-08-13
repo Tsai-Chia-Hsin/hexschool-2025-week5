528789a29cb862708f6dd0bf82ecb4279113965a
Tsai <c751c021@gmail.com>
2025-08-13 16:31:03 +0800

    bootstrap commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/hexschool-2025-week-5/',
+  base: '/hexschool-2025-week5/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
